4a6578440d7bec54caba3d2d702dc2c0e4deaf87
Johanovský Karel <johanovsky@prumyslovkajihlava.cz>
2025-09-22 12:58:27 +0200

    20250922 Filtr+Strankovani

--- IT4B_IT/React_FiltrFilmy/src/App.jsx
+++ IT4B_IT/React_FiltrFilmy/src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import {
-  Container
+  Box,
+  Container, FormControl, InputLabel, MenuItem, Paper, 
+  Select, Table, TableBody, TableCell, TableContainer, 
+  TableHead, TablePagination, TableRow, TextField
 } from "@mui/material";
 
 function App() {
@@ -17,6 +20,25 @@ function App() {
 
   //budeme potreboavat pole unikatnich zanru
   const [uniqZanry, setUniqZanry] = useState([]);
+
+  //usestates pro strankovani
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(20);
+
+  //usestates pro razeni
+  const [orderBy, setOrderBy] = useState(""); //bude obsahovat jmeno sloupce podle ktereho radime
+  const [orderDirection, setOrderDirection] = useState("asc"); //bude obsahovat smer razeni 
+
+  //co se stane, kdyz zmenim stranku
+  const pageChangeHandler = (event, newPage) => {
+    setPage(newPage);
+  }
+
+  //co se stane, kdyz zmenim pocet zaznau na strance
+  const rowsPerPageChangeHandler = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0); //navrat na prvni stranku
+  }
 
   useEffect(() => {
     fetch(API_URL)
@@ -49,10 +71,106 @@ function App() {
     })
   };
 
+  //funkce ktera spocita pocet vyfiltrovanych zaznamu
+  const getFilteredDataCount = () => {
+    return getFilteredData().length;
+  }
+
+  //funkce ktera vyrizne ze vsech dat, jenom ta ktera se budou zobrazovat
+  const getFilteredDataSlice = () => {
+    //potrebuju vsechna data - uz vyfiltrovana
+    const all_filtered = getFilteredData();
+
+    //some magic to do...
+
+
+    //vratime pouze vyrez z vsech
+    return all_filtered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+  }
+
   return (
     <Container>
       <h1>React Filter Filmy</h1>
+
+      {/* Filtrovani */}
+
+      <Box sx={{ display: "flex", gap: 1, flexWrap: "wrap", mb: 2}}>
+
+        <TextField
+          label="Filtrovani podle nazvu"
+          value={hledJmeno}
+          onChange={(e) => setHledJmeno(e.target.value)}
+        />
+
+        <FormControl sx={{minWidth: 160}}>
+          <InputLabel>Žánr</InputLabel>
+          <Select
+            label="Žánr"
+            value={hledZanr}
+            onChange={(e) => setHledZanr(e.target.value)}
+          >
+            <MenuItem value="">Vše</MenuItem>
+            {
+              uniqZanry.map((item) => (
+                <MenuItem value={item}>{item}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+
+      </Box>
+
+      {/* Tabulka s daty */}
+
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                Název
+              </TableCell>
+              <TableCell>
+                Rok vydání
+              </TableCell>
+              <TableCell>
+                Žánr
+              </TableCell>
+              <TableCell>
+                Hodnocení
+              </TableCell>
+              <TableCell>
+                Délka filmu
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              getFilteredDataSlice().map((item) => (
+                <TableRow>
+                  <TableCell>{item.nazev}</TableCell>
+                  <TableCell>{item.rok}</TableCell>
+                  <TableCell>{item.zanr}</TableCell>
+                  <TableCell>{item.hodnoceni}</TableCell>
+                  <TableCell>{item.delka}</TableCell>
+                </TableRow>
+              ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
       
+      {/* Strankovani */}
+
+      <TablePagination 
+        component="div"
+        count={getFilteredDataCount()}
+        page={page}
+        onPageChange={pageChangeHandler}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={rowsPerPageChangeHandler}
+        rowsPerPageOptions={[5, 10, 20]}
+      />
+
     </Container>
   )
 }
