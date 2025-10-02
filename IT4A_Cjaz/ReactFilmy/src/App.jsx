52d9ce05cdd32e4f7cc2b522658a95c5e2d853ad
Johanovský Karel <johanovsky@prumyslovkajihlava.cz>
2025-10-02 12:51:10 +0200

    20251002

--- IT4A_Cjaz/ReactFilmy/src/App.jsx
+++ IT4A_Cjaz/ReactFilmy/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { Box, Container, FormControl, InputLabel, MenuItem, Paper, Select, Table, TableBody, 
   TableCell, TableContainer, TableHead, TablePagination, TableRow, 
+  TableSortLabel, 
   TextField } from '@mui/material';
 
 function App() {
@@ -23,6 +24,10 @@ function App() {
   //usestates kvuli strankovani
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPage] = useState(20);
+
+  //usestates kvuli razeni
+  const [orderBy, setOrderBy] = useState("");
+  const [orderDirection, setOrderDirection] = useState("asc");
 
   //Nacteni dat z API do pole dat
   useEffect(() => {
@@ -57,6 +62,57 @@ function App() {
   //Funkce ktera spocita pocet vyfiltrovanych zaznamu ze vsech dat
   function getFilterredDataCount() {
     return getFilterredData().length;
+  }
+
+  //Metoda pro vytahnuti casti dat ze vsech
+  const getFilterredDataSlice = () => {
+    //vytahnu vsechna data
+    const all = getFilterredData();
+
+    //radime - pokud je pozadovano
+    all.sort((a, b) => {
+      const aVal = a[orderBy] ?? "";
+      const bVal = b[orderBy] ?? "";
+
+      const obeJsouCisla = (!isNaN(aVal) && !isNaN(bVal));
+
+      if(obeJsouCisla) {
+        //budeme radit jako cisla
+        return orderDirection === "asc" 
+          ? (Number(aVal) - Number(bVal))
+          : (Number(bVal) - Number(aVal));
+      } else {
+        //budeme radit jako retezce
+        return orderDirection === "asc"
+          ? String(aVal).localeCompare(String(bVal))
+          : String(bVal).localeCompare(String(aVal));
+      }
+
+
+    });
+
+    //vyriznu z nich aktualni cast dat
+    return all.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+  }
+
+  //metoda pro zmenu stránky
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  }
+
+  //metoda pro zmenu poctu zaznamu na strance
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPage(parseInt(event.target.value, 10));
+    setPage(0); //reset na  stranku
+  }
+
+  //metoda pro zmenu sloupce, nebo smeru razeni
+  const handleSort = (column) => {
+    //nastavime sloupec podle ktereho budeme radit
+    setOrderBy(column);
+
+    const isAsc = ((orderBy === column) && (orderDirection === "asc"));
+    setOrderDirection(isAsc ? "desc" : "asc");
   }
 
   return (
@@ -101,21 +157,45 @@ function App() {
                 Název
               </TableCell>
               <TableCell>
-                Rok vydání
+                <TableSortLabel
+                  active={orderBy === "rok"}
+                  direction={orderBy === "rok" ? orderDirection : "asc"}
+                  onClick={() => handleSort("rok")}
+                >
+                  Rok vydání
+                </TableSortLabel>
               </TableCell>
               <TableCell>
-                Žánr
+                <TableSortLabel
+                  active={orderBy === "zanr"}
+                  direction={orderBy === "zanr" ? orderDirection : "asc"}
+                  onClick={() => handleSort("zanr")}
+                >
+                  Žánr
+                </TableSortLabel>
               </TableCell>
               <TableCell>
-                Hodnocení
+                <TableSortLabel
+                  active={orderBy === "hodnoceni"}
+                  direction={orderBy === "hodnoceni" ? orderDirection : "asc"}
+                  onClick={() => handleSort("hodnoceni")}
+                >
+                  Hodnocení
+                </TableSortLabel>
               </TableCell>
               <TableCell>
-                Délka filmu
+                <TableSortLabel
+                  active={orderBy === "delka"}
+                  direction={orderBy === "delka" ? orderDirection : "asc"}
+                  onClick={() => handleSort("delka")}
+                >
+                  Délka filmu
+                </TableSortLabel>
               </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {getFilterredData().map((item) => (
+            {getFilterredDataSlice().map((item) => (
               <TableRow>
                 <TableCell>{item.nazev}</TableCell>
                 <TableCell>{item.rok}</TableCell>
