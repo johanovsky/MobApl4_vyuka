0b11bab1855876acb50a943138646f7ab878a8f9
Johanovský Karel <johanovsky@prumyslovkajihlava.cz>
2025-11-13 13:48:51 +0100

    20251113

--- IT4A_Cjaz/ReactColorMixer/src/App.jsx
+++ IT4A_Cjaz/ReactColorMixer/src/App.jsx
@@ -10,12 +10,71 @@ function App() {
 
   async function saveColor() 
   {
-
+    try {
+      //data ktera budu ukladat
+      const data = { red, green, blue};
+      //nastaveni ukladaciho okna
+      const opt = {
+        //predvyplnene jmeno
+        suggestedName: "barva.json",
+        //podporovane typy souboru
+        types: [
+          {
+            description: "JSON soubor",
+            accept: {"application/json": [".json"]},
+          },
+        ]
+      }
+      //zobrazime ukladaci okno
+      //okno na vraci odkaz na soubor
+      const file = await window.showSaveFilePicker(opt);
+      //pripojime zapisovaci proud
+      const zapisovac = await file.createWritable();
+      //zapiseme data
+      await zapisovac.write(JSON.stringify(data, null, "\t"));
+      //uzavreme zapisovaci proud
+      await zapisovac.close();
+    } catch(err) {
+      console.log(err);
+      alert("Ulozeni se nezdarilo");
+    }
   }
 
   async function loadColor() 
   {
+    try 
+    {
+      /* destruovani pole */
+      //const [file] = await window.showOpenFilePicker();
 
+      /* bez destruovani pole */
+      const files = await window.showOpenFilePicker();
+      const file = files[0];
+
+      //z odkazu ziskame soubor
+      const opravduFile = await file.getFile();
+      //soubor precteme do stringu
+      const text = await opravduFile.text();
+      //ten string precteme jako JSON
+      const data = JSON.parse(text);
+
+      setRed(data.red ?? 0);
+      //setGreen(data.green ?? 0);
+      if(data.green == null) 
+      {
+        setGreen(0);
+      }
+      else 
+      {
+        setGreen(data.green);
+      }
+      setBlue(data.blue ?? 0);
+    } 
+    catch(err) 
+    {
+      console.log(err);
+      alert("Nacteni se nezdarilo");
+    }
   }
 
   return (
