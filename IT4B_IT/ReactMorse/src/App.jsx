f338b167fcf20d88b3626b0403d7109101a6b89c
Johanovský Karel <johanovsky@prumyslovkajihlava.cz>
2026-02-09 13:38:55 +0100

    20260209

--- IT4B_IT/ReactMorse/src/App.jsx
+++ IT4B_IT/ReactMorse/src/App.jsx
@@ -14,6 +14,11 @@ function App() {
   const VIB_CARKA = 500;
   const VIB_PAUSE = 350;
 
+  const BEEP_TECKA = VIB_TECKA;
+  const BEEP_CARKA = VIB_CARKA;
+  const BEEP_PAUSE = VIB_PAUSE;
+  const BEEP_FREQ = 500;
+
   function prevodMorse() {
     let vysledek = "";
     //projdeme text znak po znaku
@@ -76,6 +81,58 @@ function App() {
     }
   }
 
+  async function beep(ms, frequency) {
+    //vytvorime audio-context
+    /*
+    //komp. version
+    const context = new (window.AudioContext || 
+      window.webkitAudioContext)();
+      */
+    const context = new window.AudioContext();
+    //pres kontext vytvorime - generator vlny (pipak)
+    const oscilator = context.createOscillator();
+    //nastavime pipak
+    //typ vlny square, sine, trinagle, sawtooth
+    oscilator.type = "sawtooth";
+    //nastavime frekvenci
+    oscilator.frequency.value = frequency;
+    //nasmerujeme pipak na reproduktor zarizeni
+    oscilator.connect(context.destination);
+    //zapneme nastaveny pipak
+    oscilator.start();
+    //nechame ho chvilku znit
+    await pockej(ms);
+    //vypneme pipak
+    oscilator.stop();
+    //uvolneni sys. prostredku
+    context.close();
+  }
+
+  async function pipatMorse() {
+    //projdeme ulozenou morseovku
+    for(let i = 0; i < morse.length; i++) {
+      //stahneme znak
+      const znak = morse[i];
+      //je-li to tecka
+      if(znak === ".") {
+        //pipneme kratce
+        await beep(BEEP_TECKA, BEEP_FREQ);
+        //pockame
+        await pockej(BEEP_PAUSE);
+      //je-li to carka
+      } else if(znak === "-") {
+        //pipneme dlouze
+        await beep(BEEP_CARKA, BEEP_FREQ);
+        //pockame
+        await pockej(BEEP_PAUSE);
+      //je-li to lomitko
+      } else if (znak === "/") {
+        //pockame
+        await pockej(2* BEEP_PAUSE);
+      }
+    }
+  }
+
   return (
     <div>
       <h1>React Morse Code</h1>
@@ -105,6 +162,11 @@ function App() {
         onClick={vibrovatMorse}
       >
         Vibrovat morseovku
+      </button>
+      <button
+        onClick={pipatMorse}
+      >
+        Pipat morseovku
       </button>
     </div>
   )
