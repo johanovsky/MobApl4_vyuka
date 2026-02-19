5fa2433674f1a03aa91a8c08fa57940c63ad7993
Johanovský Karel <johanovsky@prumyslovkajihlava.cz>
2026-02-19 13:59:39 +0100

    20260219

--- IT4A_Java/React_Morse/src/App.jsx
+++ IT4A_Java/React_Morse/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -7,6 +7,10 @@ function App() {
   const [text, setText] = useState("");
   //tohle bude pro morseovku
   const [morse, setMorse] = useState("");
+
+  //useRefs
+  //odkaz na stopu kamery, sice ho nikde nezobrazuju, ale potrebuju ho kvuli obsluze ledky
+  const videoRef = useRef(null);
 
   //pole stringu morseovy abecedy
   const morseAlphabeth = [ ".-", "-...", "-.-.", "-..", ".", "..-.",
@@ -18,6 +22,17 @@ function App() {
     const VIB_TECKA = 250;
     const VIB_CARKA = 500;
     const VIB_PAUSE = 350;
+
+    //konstanty pro pipani
+    const BEEP_TECKA = VIB_TECKA;
+    const BEEP_CARKA = VIB_CARKA;
+    const BEEP_PAUSE = VIB_PAUSE;
+    const BEEP_FREQ = 440;
+
+    //konstanty pro blikani
+    const LED_TECKA = VIB_TECKA;
+    const LED_CARKA = VIB_CARKA;
+    const LED_PAUSE = VIB_PAUSE;
 
   //funkce pro prevod textu do morseovky
   function prevodMorse() {
@@ -81,7 +96,139 @@ function App() {
         await pockej(VIB_PAUSE);
       }
     }
+  }
+  
+  //cas [ms], freq [Hz]
+  async function beep(ms, frequency) {
+    //vytahnu si audio-kontext prohlizece
+    //kompatibilni verze
+    //const context = new (window.AudioContext || window.webkitAudioContext)();
+    const context = new window.AudioContext();
+    //vytvorim generator vlny
+    const oscilator = context.createOscillator();
+    //nastavime oscilator
+    //typ vlny - square = obdelnik, sine, triangle, sawtooth
+    oscilator.type = "square";
+    //nastavime frekvenci
+    oscilator.frequency.value = frequency;
+    //pripojime oscilator na reproduktor
+    oscilator.connect(context.destination);
 
+    //spustime oscilator
+    oscilator.start();
+    //chvilku ho nechame znit
+    await pockej(ms);
+    //oscilator zastavime
+    oscilator.stop();
+
+    //uvolneni syst. prostredku
+    context.close();
+  }
+
+  async function pipatMorse() {
+    //projdeme cely string s morseovkou
+    for(let i = 0 ; i < morse.length; i++) {
+      //vytahnu si znak
+      const znak = morse[i];
+      //je-li to tecka
+      if(znak === ".") {
+        //je to tecka -> pipneme kratce
+        await beep(BEEP_TECKA, BEEP_FREQ);
+        //chvilku pockame
+        await pockej(BEEP_PAUSE);
+      }
+      //je-li to carka
+      else if(znak === "-") {
+        //je to carka -> pipneme dlouze
+        await beep(BEEP_CARKA, BEEP_FREQ);
+        //chvilku pockame
+        await pockej(BEEP_PAUSE);
+      }
+      //je-li to lomitko
+      else if(znak === "/") {
+        //delsi chvilku pockame
+        await pockej(2 * BEEP_PAUSE);
+      }
+    }
+  }
+
+  async function pripravKameru() {
+    //kontrola jestli kamera uz neni aktivni
+    if(videoRef.current !== null) {
+      //kamera uz je ready, nic dalsiho nepotrebuji
+      return;
+    }
+    //kamera neni ready -> pozadame o pristup k zadni kamere
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {facingMode: "environment"}
+    });
+    //stahnu si video-stopu a ulozim do reference
+    videoRef.current = stream.getVideoTracks()[0];
+  }
+
+  //true = rozsvitit / false zhasnout
+  async function setCameraLed(state) {
+    ///kontrola
+    if(videoRef.current === null) {
+      //kamera neni ready -> konec
+      return;
+    }
+    //kamera je ready -> rozsvitime / zhasneme
+    await videoRef.current.applyConstraints({
+      advanced: [{torch: state}]
+    });
+  }
+
+  function vypniKameru() {
+    if(videoRef.current !== null) {
+      //kamera jede
+      //zastavime stream
+      videoRef.current.stop();
+      //video-ref nastavime zpet do null
+      videoRef.current = null;
+    }
+  }
+
+  async function blikatMorse() {
+    try {
+      //nejprve pripravime kameru
+      await pripravKameru();
+
+      //prochazime celou morseovku
+      for(let i = 0; i < morse.length; i++) {
+        //vytahnu znak
+        const znak = morse[i];
+        if(znak === ".") {
+          //je to tecka
+          //rozsvit
+          await setCameraLed(true);
+          //pockame tecku
+          await pockej(LED_TECKA);
+          //zhasneme
+          await setCameraLed(false);
+          //pockame pauzu
+          await pockej(LED_PAUSE);
+        } else if(znak === "-") {
+          //je to carka
+          //rozsvit
+          await setCameraLed(true);
+          //pockame carku
+          await pockej(LED_CARKA);
+          //zhasneme
+          await setCameraLed(false);
+          //pockame pauzu
+          await pockej(LED_PAUSE);
+        } else if(znak === "/") {
+          //je to lomitko
+          await pockej(2 * LED_PAUSE);
+        }
+      }
+    } catch(e) {
+        alert("LED neni dostupna");
+    } finally {
+      //vzdy vypneme kameru
+      vypniKameru();
+    }
   }
 
   return (
@@ -113,6 +260,16 @@ function App() {
         onClick={vibrovatMorse}
       >
         Vibrovat morseovku
+      </button>
+      <button
+        onClick={pipatMorse}
+      >
+        Pipat morseovku
+      </button>
+      <button
+        onClick={blikatMorse}
+      >
+        Blikat morseovku
       </button>
     </div>
   )
